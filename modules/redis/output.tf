4d5b3a29e99d8b03cdd30f7ef7996e6da1491950
narandhran <narandhran@gmail.com>
2024-12-17 22:08:22 +0530

    remove unnecessary output var

--- modules/redis/output.tf
+++ modules/redis/output.tf
@@ -2,8 +2,3 @@ output "redis_endpoint" {
   value       = aws_elasticache_cluster.redis.configuration_endpoint
   description = "The endpoint of the Redis cluster"
 }
-
-variable "instance_class" {
-  description = "Instance class for Redis (e.g., cache.t3.micro)"
-  type        = string
-}
