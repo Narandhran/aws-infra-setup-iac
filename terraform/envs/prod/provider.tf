70981fe31713efbfabf25fa801be8a77fe288c7b
narandhran <narandhran@gmail.com>
2024-12-18 18:47:52 +0530

    prod code added

--- terraform/envs/prod/provider.tf
+++ terraform/envs/prod/provider.tf
@@ -1,19 +1,19 @@
 terraform {
-    required_version = ">= 1.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "> 4.0"
-        }
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "> 4.0"
     }
+  }
 }
 
 provider "aws" {
-    region = var.region
-    default_tags {
-      tags = {
-        Env = "dev"
-        CreatedBy = "Terraform"
-      }
+  region = var.region
+  default_tags {
+    tags = {
+      Env       = "dev"
+      CreatedBy = "Terraform"
     }
+  }
 }
