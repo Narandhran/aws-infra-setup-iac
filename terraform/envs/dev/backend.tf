d724b14171ea89bb7af1343578fcfeb1eb3cf5f3
narandhran <narandhran@gmail.com>
2024-12-18 10:29:22 +0530

    Backend - dynamic updte

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -1,7 +1,4 @@
 terraform {
   backend "s3" {
-    bucket = "bckt-tf-state-b1os"    # Replace with your bucket name
-    key    = "dev/terraform.tfstate" # Path inside the bucket
-    region = "eu-west-1"             # Enable encryption
   }
 }
